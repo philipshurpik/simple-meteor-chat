2d97d483b59a760b4e049932c4f08f643e838294
Philip Shurpik <Philip.Shurpik@KruscheCompany.com>
2015-03-17 11:42:28 +0200

    Added styles from meteor tasks example Added collection of rooms

--- simple-meteor-chat.js
+++ simple-meteor-chat.js
@@ -1,9 +1,14 @@
+Rooms = new Mongo.Collection("rooms");
+
 if (Meteor.isClient) {
     Template.body.helpers({
-        rooms: [
-            { text: "Room 1" },
-            { text: "Room 2" },
-            { text: "Room 3" }
-        ]
+        rooms: function () {
+            return Rooms.find({});
+        }
     });
+    setTimeout(function() {
+        if (Rooms.find({}).count() === 0) {
+            Rooms.insert({text: "Demo room!"});
+        }
+    }, 1000);
 }
